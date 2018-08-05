2489273e7053936ecc97c72b5d568f737271e58b
Dolois <dloisel@sfr.fr>
2018-08-05 18:38:12 +0200

    j'ai complété le programme Application.java

--- src/Application.java
+++ src/Application.java
@@ -3,15 +3,20 @@ public class Application {
 
 	public static void main(String[] args)
 	{
-		ImplInterface1 int1 = new ImplInterface1();
+		ImplInterface1 note = new ImplInterface1();
+		note.v1 = 14;
+		note.v2 = 12;
 		
-		int1.v1 = 14;
-		int1.v2 = 12;
+		// afficher la note mini et la note maxi
+		System.out.println("Note mini : " + note.minimum());
+		System.out.println("Note maxi : " + note.maximum());
 		
-		System.out.println(int1.minimum());
-		System.out.println(int1.maximum());
-		
+		ImplInterface2 taille = new ImplInterface2();
+		taille.v1 = 173;
+		taille.v2 = 183;
+		taille.v3 = 188;
 
+		System.out.println("la taille max est de : " + taille.maximum() + " cm .");
 	}
 
 }
