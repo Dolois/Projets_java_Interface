2b984b978312a6af6ed680a285669819a4750e7e
Dolois <dloisel@sfr.fr>
2018-08-06 21:04:37 +0200

    meilleurs gestion des getters et setters

--- src/Application.java
+++ src/Application.java
@@ -3,20 +3,28 @@ public class Application {
 
 	public static void main(String[] args)
 	{
+		// créer une instance note de la classe ImplInterface1
 		ImplInterface1 note = new ImplInterface1();
-		note.v1 = 14;
-		note.v2 = 12;
+		note.setV1(14);
+		note.setV2(12);
 		
 		// afficher la note mini et la note maxi
 		System.out.println("Note mini : " + note.minimum());
 		System.out.println("Note maxi : " + note.maximum());
 		
+		note.setV1(17);
+		note.setV2(20);
+		
+		System.out.println();
+		System.out.println("Note mini : " + note.getV1());
+		System.out.println("Note maxi : " + note.getV2());
+		
 		ImplInterface2 taille = new ImplInterface2();
-		taille.v1 = 173;
-		taille.v2 = 183;
-		taille.v3 = 188;
+		taille.setV1(173);
+		taille.setV2(183);
+		taille.setV3(188);
 
+		System.out.println();
 		System.out.println("la taille max est de : " + taille.maximum() + " cm .");
 	}
-
 }
--- src/ExempleInterface.java
+++ src/ExempleInterface.java
@@ -1,6 +1,18 @@
 
+// l'interface s'est l'ensemble 
+// des signatures des méthodes communes de l'application
 public interface ExempleInterface 
 {
+	// la signature de la méthode minimum
 	public int minimum();
+	
+	// la signature de la méthode maximum
 	public int maximum();
+	
+	// signatures des méthodes getters et setters communes
+	public int getV1();
+	public int getV2();
+	
+	public void setV1(int v1);
+	public void setV2(int v2);
 }
